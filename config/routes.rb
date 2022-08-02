05d530050c507a3823bf74827426583118e1b6bb
hisa1020 <hisa1020gueen@gmail.com>
2022-08-02 22:16:52 +0900

    clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   root to: 'top#index'
-  
+
   devise_for :users, skip: [:passwords], controllers:{
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
+
   get 'users/account', to: 'users#show'
   get 'users/profile', to: 'users#profile', as:'users_profile'
-  resources :users, only: [:show, :update]
   
+  resources :users, only: [:show, :update]
+
   resources :rooms do
     collection do
       get 'posts'
@@ -16,10 +18,6 @@ Rails.application.routes.draw do
     end
   end
 
-  
-  
-  
   resources :reservations, only: [:index, :new, :create, :show, :destroy] 
   get 'reservations/index'
-
 end
